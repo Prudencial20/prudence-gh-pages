3f6e2d24b8e74b3124397580981c8972d223feda
Prudencial20 <ebenprudence1996@gmail.com>
2022-08-03 11:13:24 +0100

    my fourth changes

--- src/submissionform.js
+++ src/submissionform.js
@@ -12,7 +12,7 @@ const SubmissionForm = () => {
         <Form.Control type="email" placeholder="Enter email" />
         <Form.Text className="text-light">
           We'll never share your email with anyone else.
-          <Form.Label className='text-light'>Email address</Form.Label>
+          {/* <Form.Label className='text-light'>Email address</Form.Label> */}
           <Form.Control type="email" placeholder="Enter comment" />
         </Form.Text>
       </Form.Group>
